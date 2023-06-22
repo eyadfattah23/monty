4644967b7f89f7d8447215996d27404b5d3b2d2e
mahmoud <mahmoud.m3798@gmail.com>
2023-06-22 01:25:42 -0700

    implement push and pall functions

---
+++ stack.c
@@ -0,0 +1,53 @@
+#include "monty.h"
+
+/**
+ * push - Pushes an element to the stack.
+ * @stack: Double pointer to the beginning of the stack.
+ * @line_number: Line number of the instruction.
+ */
+void push(stack_t **stack, unsigned int line_number)
+{
+	int n;
+
+	if (!stack)
+		return;
+
+	n = atoi(line + 4);
+
+	stack_t *new_node = malloc(sizeof(stack_t));
+
+	if (!new_node)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	new_node->n = n;
+	new_node->prev = NULL;
+
+	if (*stack)
+	{
+		(*stack)->prev = new_node;
+		new_node->next = *stack;
+	}
+	else
+	{
+		new_node->next = NULL;
+	}
+	*stack = new_node;
+}
+
+/**
+ * pall - Prints all the values on the stack, starting from the top.
+ * @stack: Pointer to the beginning of the stack.
+ * @line_number: Line number of the instruction.
+ */
+void pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current = *stack;
+
+	while (current)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+	}
+}
