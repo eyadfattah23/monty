0f745ce570ba5e045c92646b2932ee863a04ceb4
mahmoud <mahmoud.m3798@gmail.com>
2023-06-22 01:55:31 -0700

    Update push

--- stack.c
+++ stack.c
@@ -7,27 +7,28 @@
  */
 void push(stack_t **stack, unsigned int line_number)
 {
+	char *arg = strtok(NULL, " \t\n");
 	int n;
 
-	if (!stack)
-		return;
-
-	n = atoi(line + 4);
-
+	if (!arg || !is_number(arg))
+	{
+		printf("L%d: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	n = atoi(arg);
 	stack_t *new_node = malloc(sizeof(stack_t));
 
 	if (!new_node)
 	{
-		fprintf(stderr, "Error: malloc failed\n");
+		printf("Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
 	new_node->n = n;
 	new_node->prev = NULL;
-
 	if (*stack)
 	{
-		(*stack)->prev = new_node;
 		new_node->next = *stack;
+		(*stack)->prev = new_node;
 	}
 	else
 	{
@@ -43,6 +44,7 @@ void push(stack_t **stack, unsigned int line_number)
  */
 void pall(stack_t **stack, unsigned int line_number)
 {
+	(void) line_number;
 	stack_t *current = *stack;
 
 	while (current)
