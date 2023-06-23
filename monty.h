47252007b10c303b895f398900b3e1d978d3adde
mahmoud <mahmoud.m3798@gmail.com>
2023-06-22 17:18:59 -0700

    update Swap

--- monty.h
+++ monty.h
@@ -45,7 +45,7 @@ void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
-void add(stack_t **stack, unsigned int line_number);
+void _add(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
 void free_stack(stack_t *stack);
 int parse_file(FILE *file, stack_t **stack);
