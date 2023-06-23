1f683d0f5be81f7e73c6d207de4fa86ced8471c3
mahmoud <mahmoud.m3798@gmail.com>
2023-06-22 17:13:22 -0700

    Implement Swap

--- stack_2.c
+++ stack_2.c
@@ -2,7 +2,7 @@
 /**
 * pop - pops the very top
 * @stack: stack given by main
-* @line_cnt: line number for error messages
+* @line_number: line number for error messages
 *
 * Return: void
 */
@@ -13,7 +13,7 @@ void pop(stack_t **stack, unsigned int line_number)
 	if (!stack || !*stack)
 	{
 		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
-		exit (EXIT_FAILURE);
+		exit(EXIT_FAILURE);
 	}
 
 	tmp = (*stack)->next;
@@ -22,4 +22,49 @@ void pop(stack_t **stack, unsigned int line_number)
 	if (!*stack)
 		return;
 	(*stack)->prev = NULL;
+}
+/**
+ * swap -  swaps data from top to previous
+ * @stack: stack given by main
+ * @line_number: ammount of lines
+ *
+ * Return: void
+ */
+void swap(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp = NULL;
+	int tmp_n = 0;
+
+	if (!stack || !*stack || !((*stack)->next))
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	tmp = *stack;
+	tmp_n = tmp->n;
+	tmp->n = tmp_n;
+
+	tmp->n = tmp->next->n;
+	tmp->next->n = tmp_n;
+}
+/**
+ * _add -  adds the first two nodes of the stack
+ * @stack: stack given by main
+ * @line_number: line counter
+ *
+ * Return: void
+ */
+void _add(stack_t **stack, unsigned int line_number)
+{
+	int result;
+
+	if (!stack || !*stack || !((*stack)->next))
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	result = ((*stack)->next->n) + ((*stack)->n);
+	pop(stack, line_number);
+	(*stack)->n = result;
 }
